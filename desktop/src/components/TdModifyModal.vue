39fbbc065f9114072a25247c4b1c7407081ef8fd
CodingTomato <12703962+CodingTomato@users.noreply.github.com>
2022-08-21 11:58:07 +0200

    Start Working on Multiple actions

--- desktop/src/components/TdModifyModal.vue
+++ desktop/src/components/TdModifyModal.vue
@@ -1,172 +1,238 @@
 <template>
   <q-dialog v-model="isModalOpen">
-    <q-card style="width: 80vw; max-width: 80vw">
-      <q-card-section>
-        <div class="text-h6">
-          {{ editMode ? t('edit_element') : t('add_new_element') }}
-        </div>
+    <q-card style="width: 80vw; max-width: 80vw; overflow-x: hidden" flat>
+      <q-item>
+        <q-item-section>
+          <q-item-label>
+            <h6 style="margin: 0">
+              {{ editMode ? t('edit_element') : t('add_new_element') }}
+            </h6></q-item-label
+          >
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+
+      <q-card-section :horizontal="elementType === 'Button'">
+        <q-card-section class="col-5">
+          <div class="row">
+            <q-select
+              filled
+              v-model="elementType"
+              class="fullWidth"
+              :options="elementOptions"
+              :label="t('element_type')"
+            />
+          </div>
+
+          <!-- Layout Name -->
+          <div class="row" v-if="elementType === 'Layout'">
+            <q-input
+              filled
+              class="fullWidth"
+              v-model="elementText"
+              label="Layout Name"
+            />
+          </div>
+
+          <!-- Button Text -->
+          <div
+            class="row"
+            v-if="elementType === 'Button' || elementType === 'Text'"
+          >
+            <q-input
+              filled
+              class="fullWidth"
+              v-model="elementText"
+              :label="t('text_emoji')"
+            />
+          </div>
+
+          <!-- Button Image -->
+          <div class="row" v-if="elementType === 'Button'">
+            <q-file
+              filled
+              class="fullWidth"
+              v-model="elementImage"
+              :label="t('image_animation')"
+              accept=".jpg, .gif, .png, image/*"
+            />
+          </div>
+
+          <!-- Button Farbe -->
+          <div
+            class="row"
+            v-if="elementType === 'Button' || elementType === 'Text'"
+          >
+            <q-field filled class="fullWidth" :label="t('color')" stack-label>
+              <template v-slot:control>
+                <input v-model="elementColor" type="color" />
+              </template>
+            </q-field>
+          </div>
+
+          <!-- Twitch Chat Channel Selector -->
+          <div class="row" v-if="elementType === 'Twitch Chat'">
+            <q-input
+              filled
+              class="fullWidth"
+              v-model="elementText"
+              :label="t('channelname')"
+            />
+          </div>
+
+          <!-- Switch layout selector -->
+          <div
+            class="row"
+            v-if="elementType === 'Button' && elementAction === 'switch_layout'"
+          >
+            <q-select
+              filled
+              class="fullWidth"
+              v-model="elementData"
+              :options="['next', 'last', ...store.availableLayoutsNames]"
+              :label="t('layout')"
+            />
+          </div>
+        </q-card-section>
+
+        <q-separator vertical v-if="elementType === 'Button'" />
+
+        <q-card-section class="col-7" v-if="elementType === 'Button'">
+          <div class="row" v-if="elementType === 'Button'">
+            <q-select
+              filled
+              class="col-10"
+              v-model="elementAction"
+              :options="actionOptions"
+              :label="t('action')"
+            />
+            <q-btn
+              square
+              color="primary"
+              class="col-2"
+              icon="add"
+              @click="addAction"
+            />
+          </div>
+
+          <q-timeline
+            color="secondary"
+            layout="dense"
+            v-if="elementType === 'Button'"
+            style="max-height: 30vh; overflow-y: auto"
+          >
+            <q-timeline-entry
+              v-for="(action, i) in elementActions"
+              :key="action.id"
+              :subtitle="`${i + 1}. ${action.type}`"
+            >
+              <template v-slot:title>
+                <!-- Data Only-->
+                <template
+                  v-if="
+                    action.type !== 'switch_layout' &&
+                    action.type !== 'discord' &&
+                    action.type !== 'twitch_chat_message'
+                  "
+                >
+                  <div class="row">
+                    <q-input
+                      filled
+                      bottom-slots
+                      dense
+                      class="fullWidth"
+                      v-model.trim="action.data"
+                      :label="`${action.type} Data`"
+                    >
+                      <template v-slot:after>
+                        <q-btn
+                          round
+                          dense
+                          flat
+                          icon="delete"
+                          @click="removeAction(i)"
+                        />
+                      </template>
+                    </q-input>
+                  </div>
+                </template>
+
+                <!-- Twitch Chat message channel names -->
+                <template v-if="action.type === 'twitch_chat_message'">
+                  <div class="row">
+                    <q-input
+                      filled
+                      dense
+                      class="fullWidth"
+                      v-model="action.data.channels"
+                      label="Twitch Kanalnamen (Kommasepariert)"
+                    />
+                  </div>
+
+                  <!-- Twitch Chat message -->
+                  <div class="row">
+                    <q-input
+                      filled
+                      bottom-slots
+                      dense
+                      class="fullWidth"
+                      v-model="action.data.message"
+                      label="Nachricht"
+                    >
+                      <template v-slot:after>
+                        <q-btn
+                          round
+                          dense
+                          flat
+                          icon="delete"
+                          @click="removeAction(i)"
+                        />
+                      </template>
+                    </q-input>
+                  </div>
+                </template>
+
+                <!-- Discord Action selector -->
+                <template v-if="action.type === 'discord'">
+                  <div class="row">
+                    <q-select
+                      filled
+                      bottom-slots
+                      dense
+                      class="fullWidth"
+                      v-model="action.data"
+                      :options="[
+                        'mute_microphone',
+                        'unmute_microphone',
+                        'toggle_microphone',
+                        'deaf_headphones',
+                        'undeaf_headphones',
+                        'toggle_headphones',
+                        'leave_voice_channel',
+                      ]"
+                      :label="t('discord')"
+                    >
+                      <template v-slot:after>
+                        <q-btn
+                          round
+                          dense
+                          flat
+                          icon="delete"
+                          @click="removeAction(i)"
+                        />
+                      </template>
+                    </q-select>
+                  </div>
+                </template>
+              </template>
+            </q-timeline-entry>
+          </q-timeline>
+        </q-card-section>
       </q-card-section>
+      <q-separator />
 
-      <q-card-section class="q-pt-none">
-        <div class="row">
-          <q-select
-            filled
-            v-model="elementType"
-            class="fullWidth"
-            :options="elementOptions"
-            :label="t('element_type')"
-          />
-        </div>
-
-        <!-- Layout Name -->
-        <div class="row" v-if="elementType === 'Layout'">
-          <q-input
-            filled
-            class="fullWidth"
-            v-model="elementText"
-            label="Layout Name"
-          />
-        </div>
-
-        <!-- Button Text -->
-        <div
-          class="row"
-          v-if="elementType === 'Button' || elementType === 'Text'"
-        >
-          <q-input
-            filled
-            class="fullWidth"
-            v-model="elementText"
-            :label="t('text_emoji')"
-          />
-        </div>
-
-        <!-- Button Image -->
-        <div class="row" v-if="elementType === 'Button'">
-          <q-file
-            filled
-            class="fullWidth"
-            v-model="elementImage"
-            :label="t('image_animation')"
-            accept=".jpg, .gif, .png, image/*"
-          />
-        </div>
-
-        <!-- Button Farbe -->
-        <div
-          class="row"
-          v-if="elementType === 'Button' || elementType === 'Text'"
-        >
-          <q-field filled class="fullWidth" :label="t('color')" stack-label>
-            <template v-slot:control>
-              <input v-model="elementColor" type="color" />
-            </template>
-          </q-field>
-        </div>
-        <div class="row" v-if="elementType === 'Button'">
-          <q-select
-            filled
-            class="fullWidth"
-            v-model="elementAction"
-            :options="actionOptions"
-            :label="t('action')"
-          />
-        </div>
-        <div
-          class="row"
-          v-if="
-            elementType === 'Button' &&
-            elementAction !== 'switch_layout' &&
-            elementAction !== 'discord' &&
-            elementAction !== 'twitch_chat_message'
-          "
-        >
-          <q-input
-            filled
-            class="fullWidth"
-            v-model="elementData"
-            :label="t('data')"
-          />
-        </div>
-
-        <!-- Twitch Chat message channel names -->
-        <div
-          class="row"
-          v-if="
-            elementType === 'Button' && elementAction === 'twitch_chat_message'
-          "
-        >
-          <q-input
-            filled
-            class="fullWidth"
-            v-model="elementTwitchChatChannels"
-            label="Twitch Kanalnamen (Kommasepariert)"
-          />
-        </div>
-
-        <!-- Twitch Chat message -->
-        <div
-          class="row"
-          v-if="
-            elementType === 'Button' && elementAction === 'twitch_chat_message'
-          "
-        >
-          <q-input
-            filled
-            class="fullWidth"
-            v-model="elementTwitchChatMessage"
-            label="Nachricht"
-          />
-        </div>
-
-        <!-- Discord Action selector -->
-        <div
-          class="row"
-          v-if="elementType === 'Button' && elementAction === 'discord'"
-        >
-          <q-select
-            filled
-            class="fullWidth"
-            v-model="elementData"
-            :options="[
-              'mute_microphone',
-              'unmute_microphone',
-              'toggle_microphone',
-              'deaf_headphones',
-              'undeaf_headphones',
-              'toggle_headphones',
-              'leave_voice_channel',
-            ]"
-            :label="t('discord')"
-          />
-        </div>
-
-        <!-- Switch layout selector -->
-        <div
-          class="row"
-          v-if="elementType === 'Button' && elementAction === 'switch_layout'"
-        >
-          <q-select
-            filled
-            class="fullWidth"
-            v-model="elementData"
-            :options="['next', 'last', ...store.availableLayoutsNames]"
-            :label="t('layout')"
-          />
-        </div>
-
-        <!-- Twitch Chat Channel Selector -->
-        <div class="row" v-if="elementType === 'Twitch Chat'">
-          <q-input
-            filled
-            class="fullWidth"
-            v-model="elementText"
-            :label="t('channelname')"
-          />
-        </div>
-      </q-card-section>
-
-      <q-card-actions align="right" class="text-teal">
+      <q-card-actions>
         <q-btn flat :label="t('cancel')" v-close-popup />
         <q-btn
           flat
@@ -226,7 +292,7 @@ const elementId = ref(0);
 const elementRowIndex = ref(0);
 const elementText = ref('');
 const elementColor = ref('');
-const elementType = ref();
+const elementType = ref('Button');
 const elementAction = ref();
 const elementData = ref('');
 const elementImage = ref('');
@@ -234,16 +300,32 @@ const elementImage = ref('');
 const elementTwitchChatChannels = ref('');
 const elementTwitchChatMessage = ref('');
 
+const elementActions = ref([]);
+
+const addAction = () => {
+  if (elementAction.value === '') return;
+
+  let dataElement = '';
+
+  if (elementAction.value === 'twitch_chat_message') {
+    dataElement = {
+      channels: '',
+      message: '',
+    };
+  }
+
+  elementActions.value.push({
+    id: Math.random(),
+    type: elementAction.value,
+    data: dataElement,
+  });
+};
+const removeAction = (index) => {
+  elementActions.value.splice(index, 1);
+};
+
 const editElement = () => {
   const tempLayout = JSON.parse(JSON.stringify(store.layout));
-
-  let newelementData = elementData.value.trim();
-  if (elementAction.value === 'twitch_chat_message') {
-    newelementData = JSON.stringify({
-      channelNames: elementTwitchChatChannels.value,
-      message: elementTwitchChatMessage.value,
-    });
-  }
 
   const newElement = {
     id: elementId.value,
@@ -253,8 +335,7 @@ const editElement = () => {
     color: elementColor.value,
     image: elementImage.value?.path || '',
     icon: '',
-    eventName: elementAction.value,
-    data: newelementData,
+    actions: elementActions.value,
   };
 
   // eslint-disable-next-line max-len
@@ -285,25 +366,19 @@ const deleteElement = () => {
 };
 
 const addNewElement = () => {
+  if (elementType.value === 'Button' && elementActions.value.length === 0)
+    return;
+
   const id = Math.floor(Math.random() * 99999);
   const tempLayout = JSON.parse(JSON.stringify(store.layout));
-
   if (elementType.value === 'Layout') {
     const newLayout = {
       name: elementText.value,
       rows: [{ elements: [] }],
     };
-
     tempLayout.layouts.push(newLayout);
   } else {
     const rowSize = tempLayout.layouts[0].rows.length;
-
-    if (elementAction.value === 'twitch_chat_message') {
-      elementData.value = JSON.stringify({
-        channelNames: elementTwitchChatChannels.value,
-        message: elementTwitchChatMessage.value,
-      });
-    }
 
     const newElement = {
       id,
@@ -313,15 +388,13 @@ const addNewElement = () => {
       color: elementColor.value,
       image: elementImage.value.path || '',
       icon: '',
-      eventName: elementAction.value,
-      data: elementData.value.trim(),
+      actions: elementActions.value,
     };
 
     tempLayout.layouts[store.currentlyVisibleLayout.index].rows[
       rowSize - 1
     ].elements.push(newElement);
   }
-
   store.layout = tempLayout;
   store.updateLayout(JSON.stringify(tempLayout));
 };
@@ -333,16 +406,8 @@ const openModal = (element) => {
     elementText.value = element.text;
     elementColor.value = element.color;
     elementType.value = element.type;
-    elementAction.value = element.eventName;
-    elementData.value = element.data;
+    elementActions.value = element.actions;
     elementImage.value = element.image;
-
-    if (elementAction.value === 'twitch_chat_message') {
-      elementTwitchChatChannels.value = JSON.parse(
-        elementData.value,
-      ).channelNames;
-      elementTwitchChatMessage.value = JSON.parse(elementData.value).message;
-    }
 
     editMode.value = true;
     elementOptions.value = ['Button', 'Twitch Chat', 'Text'];
@@ -355,8 +420,7 @@ const openModal = (element) => {
     elementAction.value = '';
     elementData.value = '';
     elementImage.value = '';
-    elementTwitchChatChannels.value = '';
-    elementTwitchChatMessage.value = '';
+    elementActions.value = [];
 
     editMode.value = false;
     elementOptions.value = ['Button', 'Twitch Chat', 'Text', 'Layout'];
